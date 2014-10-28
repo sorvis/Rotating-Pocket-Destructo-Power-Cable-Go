3c769fe7aa40bc9f4266ceec1eb691cac1860be4
Steven Orvis <steven@trappingpa.com>
2014-10-28 19:19:07 -0400

    Renamed fanimal to favoriteAnimal

--- Myjob.py
+++ Myjob.py
@@ -4,17 +4,17 @@ job= input("What's your job? ")
 #PRINT "myjob1"? That's a stupid job! What's your favorite animal? "MYjob2"
 print(job+" That's a stupid job") 
 
-fanimal= input("What's your favorite animal? ")
+favoriteAnimal= input("What's your favorite animal? ")
 
 #PRINT "myjob2"? You should train those for a living! It's better than being some, what? "MYjob1"!? Besides which, your boss could likely hire a "MYjob2" to be a "MYjob1"!
-print (fanimal+ "?! You should train those for a living! It's better than being some "+ job+"!")
+print (favoriteAnimal+ "?! You should train those for a living! It's better than being some "+ job+"!")
 advice =input ("Do you like receiving my advice? Y/N ")
 if advice== "Y":
     print("Really? Only someone working as "+job+" would need my advice that badly.")
 elif advice== "N":
-    print("Too bad, suck it up, you're just a " +job+" who loves "+ fanimal)
+    print("Too bad, suck it up, you're just a " +job+" who loves "+ favoriteAnimal)
 else:
-    print("Did you not understand the question you idiotic "+job+" who is not as intelligent as the beloved " +fanimal)
+    print("Did you not understand the question you idiotic "+job+" who is not as intelligent as the beloved " +favoriteAnimal)
 
 while True:
     ignorance=input ("Do you know how ignorant everyone else finds you? Y/N? ")
